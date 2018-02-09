52b04ccb53edb0c84bd025bc586fc04642e7e9f4
Stephen Schroeder <schrode50@gmail.com>
2018-02-08 22:58:06 -0800

    photo thumb 2 full and back done

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ $('.neat').click(() => {
 $('.back').click(() => {
   document.location.href = 'main.html';
 })
+
+$('.snaps').find('img').bind('click', function() {
+  var extensionMatch = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gmi;
+  var src = $(this).attr('src');
+  var temp = src.match(extensionMatch);
+
+  src.includes('.thumb') ? src = src.replace(/.thumb./, '.') : src = src.replace(/[0-9a-z]+$/, 'thumb' + temp);
+
+  $(this).attr('src', src);
+})
